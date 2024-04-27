8da756a059b78fab750c68661081d527bf6501ee
jorgecao84 <151456992+jorgecao84@users.noreply.github.com>
2024-04-26 20:04:04 -0400

    Update model_app.py

--- model_app.py
+++ model_app.py
@@ -12,3 +12,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import SelectFromModel
 from sklearn.utils import resample
 from xgboost import XGBClassifier
+
+st.write("Covid Care Priority Prediction Model")
+
+filename = st.file_uploader("Upload a file", type={"csv"})
+if filename is not None:
+    dataset = pd.read_csv(filename)
+    st.write(dataset.describe())
+    chart_df = {'feature': [], 'average': [], 'label': []}
+    for label in list(set(dataset["label"].values)):
+        for column_name in dataset.columns:
+            if column_name != "label":
+                chart_df['feature'].append(column_name)
+                chart_df['average'].append(dataset[dataset["label"]==label][column_name].mean())
+                chart_df['label'].append(label)
+    pd.DataFrame(chart_df)
+fig = px.line(chart_df, x="feature", y="average",color="label")
+st.plotly_chart(fig,use_container_width=True)
