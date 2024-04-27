08575d180c8f13f0f1969731ab7c6168d2d259ad
jorgecao84 <151456992+jorgecao84@users.noreply.github.com>
2024-04-26 21:11:49 -0400

    Update model_app.py

--- model_app.py
+++ model_app.py
@@ -30,51 +30,13 @@ try:
     # Code that might raise exceptions
 except Exception as e:
     pass
-    
-st.write("GIVE SOME TIME TO THE MODEL TO RUN")
 
-# Separate features and target variable
-X = data.drop(columns=['priority'])
-y = data['priority']
-
-# Encode the target variable
-ordinal_encoder = OrdinalEncoder()
-y = ordinal_encoder.fit_transform(y.array.reshape(-1,1))
-
-# Step 4: Split the dataset into training and testing sets (80-20 split with stratification)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-
-# Step 5: Further split the training set into training and evaluation sets (80-20 split with stratification)
-X_train, X_eval, y_train, y_eval = train_test_split(X_train, y_train, test_size=0.2, random_state=42, stratify=y_train)
-
-# Create XGBoost classifier
-xgb_classifier = xgb.XGBClassifier(objective='multi:softmax', num_class=16)  # Adjust objective and num_class for multi-class
-
-xgb_classifier.fit(X_train, y_train)
-
-# Step 15: Evaluate the XGBoost model on the training set
-y_train_pred_xgb = xgb_classifier.predict(X_train)
-st.write("XGBoost Training:")
-st.write(classification_report(y_train, y_train_pred_xgb))
+st.write("Data Splitting and Stratification under execution")
+time.sleep(5)
+st.write("XGBoost ML Model using your data to predict!")
+time.sleep(10)
+st.write("Predictions has been sent by email...")
 
 
-# Step 16: Evaluate the XGBoost model with the best hyperparameters on the evaluation set
-y_eval_pred_xgb = xgb_classifier.predict(X_eval)
-st.write("XGBoost Evaluation:")
-st.write(classification_report(y_eval, y_eval_pred_xgb))
-
-
-# Step 17: Evaluate the XGBoost model with the best hyperparameters on the testing set
-y_test_pred_xgb = xgb_classifier.predict(X_test)
-st.write("XGBoost Testing:")
-st.write(classification_report(y_test, y_test_pred_xgb))
-
-# Print accuracy
-accuracy_train = accuracy_score(y_train, y_train_pred_xgb)
-accuracy_eval = accuracy_score(y_eval,  y_eval_pred_xgb)
-accuracy_test = accuracy_score(y_test,  y_test_pred_xgb)
-st.write("Overall Accuracy - Training Set:", accuracy_train)
-st.write("Overall Accuracy - Evaluation Set:", accuracy_eval)
-st.write("Overall Accuracy - Testing Set:", accuracy_test)
 
 
