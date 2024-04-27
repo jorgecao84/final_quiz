7cb23ce210e52d992e3092a97170628c4da29f61
jorgecao84 <151456992+jorgecao84@users.noreply.github.com>
2024-04-26 21:21:33 -0400

    Update model_app.py

--- model_app.py
+++ model_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 import xgboost as xgb
 from sklearn.model_selection import train_test_split, GridSearchCV
